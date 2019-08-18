e3d80a37f15ced8f27b2d660999521f2b365fa3e
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-08-18 08:02:16 +0300

    Added projects
    import_categories
    import_categori

---
+++ 03.Import_Categories/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using ProductShop.Data;
+using ProductShop.Models;
+using System;
+using System.Xml.Linq;
+
+namespace _03.Import_Categories
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputXml = @"C:\Users\thinkpad\source\repos\XMLProcessing\ProductShopDatabase\Datasets\categories.xml";
+
+            using (var context = new ProductShopContext())
+            {
+                var result = ImportCategories(context, inputXml);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            XDocument xmlDoc = XDocument.Load(inputXml);
+            int imported = 0;
+            var categories = xmlDoc.Root.Elements();
+
+            foreach (var categorie in categories)
+            {
+                var contextCategorie = new Category()
+                {
+                    Name = categorie.Element("name").Value
+                };
+
+                context.Categories.Add(contextCategorie);
+                context.SaveChanges();
+                imported += 1;
+            }
+
+            return $"Successfully imported {imported}";
+        }
+    }
+}
---
+++ 04.Import_Categories_and_Products/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using ProductShop.Data;
+using ProductShop.Models;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace _04.Import_Categories_and_Products
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputXml = @"C:\Users\thinkpad\source\repos\XMLProcessing\ProductShopDatabase\Datasets\categories-products.xml";
+
+            using (var context = new ProductShopContext())
+            {
+                var result = ImportCategoryProducts(context, inputXml);
+
+                Console.WriteLine(result);
+            }
+        }
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            XDocument xmlDoc = XDocument.Load(inputXml);
+            int imported = 0;
+            var categoriesProducts = xmlDoc.Root.Elements();
+
+            foreach (var categorieProduct in categoriesProducts)
+            {
+                if (categorieProduct.Elements().Count() < 2)
+                {
+                    continue;
+                }
+
+                var contextCategorieProduct = new CategoryProduct()
+                {
+                    CategoryId = int.Parse(categorieProduct.Element("CategoryId").Value),
+                    ProductId = int.Parse(categorieProduct.Element("ProductId").Value)
+                };
+
+                context.CategoryProducts.Add(contextCategorieProduct);
+                context.SaveChanges();
+                imported += 1;
+            }
+
+            return $"Successfully imported {imported}";
+        }
+    }
+}
