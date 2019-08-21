6693c6539f62ae2966b2bb79de442eda328fcc72
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-08-21 12:59:16 +0300

    Added CategoriesByProductsCount

---
+++ 07.Categories_By_Products_Count/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using ProductShop.Data;
+using System;
+using System.Linq;
+
+namespace _07.Categories_By_Products_Count
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var context = new ProductShopContext())
+            {
+                string result = GetCategoriesByProductsCount(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories = context.Categories
+                .Select(c => new
+                {
+                    name = c.Name,
+                    countProducts = c.CategoryProducts.Count,
+                    average = c.CategoryProducts.Average(p => p.Product.Price),
+                    totalPrice = c.CategoryProducts.Sum(p => p.Product.Price)
+                })
+                .OrderBy(p => p.countProducts)
+                .ThenBy(p => p.totalPrice)
+                .ToList();
+            ;
+            return "";
+        }
+    }
+}
